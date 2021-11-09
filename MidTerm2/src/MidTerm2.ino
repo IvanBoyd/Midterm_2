9454d787b027067fa88ad4532a36272cf1842234
Ivan Boyd <iboyd3@cnm.edu>
2021-11-09 16:04:33 -0700

    fist pile of midterm_2 files - oh yah!

---
+++ MidTerm2/src/MidTerm2.ino
@@ -0,0 +1,104 @@
+/*
+ * Project MidTerm2
+ * Description: 
+ *  1. Int 2N3906 Emitter Follower & Relay & BME w Disp
+ *  2. Publish soil moisture & room env data to new dashboard
+ *  3. Auto water plant when soil is too dry (pump only ~1/2 second)
+ *  4. Int a button on dashboard that manually waters the plant
+ * Author:      Ivan Boyd
+ * Date:        11/08/21
+ * History: <-L14_02_v2_Moisture.ino
+ */
+
+// setup() runs once, when the device is first turned on.
+#include <Adafruit_BME280.h>                // temp, pressure & humidity
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"               // OLED display
+#define OLED_RESET D4
+Adafruit_SSD1306 display(OLED_RESET);
+
+const int SOIL_MOIST_PIN = 19;
+int soilMoistVal = 0;
+
+// Set up Adafruit_BME280 Environment for temp, pressure & humidity
+Adafruit_BME280 bme;      // for the I2C device
+float tempC,          tempF,        tempinHg,
+      pressPA,        humidRH,      fiveMinAvg;
+bool  bmeStatus;
+byte  hexAddress=0x76; 
+
+String DateTime, TimeOnly;
+
+ // SYSTEM_MODE(AUTOMATIC);          // Default if no SYSTEM_MODE included
+     SYSTEM_MODE(SEMI_AUTOMATIC);     // Uncomment if using without Wifi
+  // SYSTEMF_MODE(MANUAL);            // Fully Manual
+void setup() {
+    pinMode(SOIL_MOIST_PIN,INPUT);
+    Time.zone(-7);          //MST = -7, MDT = -6
+    Particle.syncTime();    // Sync time with Particle Cloud
+    Serial.begin(9600);
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+    display.display(); // show splashscreen
+    delay(2000);
+    display.clearDisplay();   // clears the screen and buffer
+
+    // draw a single pixel
+    display.drawPixel(10, 10, WHITE);
+    display.display();
+    delay(2000);
+    display.clearDisplay();
+    runBMEchk();        // check temp module BME280
+
+
+}
+
+void loop() {
+
+   soilMoistVal = analogRead(SOIL_MOIST_PIN);
+   tempF = CtoF(bme.readTemperature());
+   Serial.printf("Moisture reg in pin: %i, Temp: %f\n", soilMoistVal, tempF);
+   delay(3000);
+      // text display tests
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Ivan Ivanovich");
+
+  display.display();
+//   delay(6000);
+  display.clearDisplay();
+
+  DateTime = Time.timeStr();      //Current Date & Time from Particle Time
+  TimeOnly = DateTime.substring(11,19);   //Ext time from datetime str
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(3,0);
+  display.printf("Date/Time:%s\n",DateTime.c_str());
+  display.printf("Time: %s\n",TimeOnly.c_str());
+  display.display();
+  Serial.printf("Date & Time is %s\n",DateTime.c_str());
+  Serial.printf("Time is %s\n",TimeOnly.c_str());
+  delay(1000);
+}                     //  *** END OF MAIN VOID LOOP ***
+
+                //      ***    F U N C T I O N S    ***
+
+void runBMEchk()  {         // check status of BME/temp and send to print
+  bmeStatus  = bme.begin(hexAddress);
+  if(bmeStatus ==  false)  {
+    Serial.printf("BME280 at address 0x%02X failed to start\n", hexAddress);
+    delay(4000);
+   }
+   else {
+    tempC   = bme.readTemperature();  //deg C
+    tempF = CtoF(tempC);
+    Serial.printf("Temperature at startup: %f \n",tempF);
+    delay(4000);
+   }
+}
+
+float CtoF(float _tempC) {     //Convert Celcius to Fahrenheit °F = (°C × 9/5) + 32//
+  float _tempF;
+  _tempF = (_tempC * 9/5) +32;
+  return _tempF;
+}
